7a8a240015893bad684deac38a9eae1671c89f98
loki992 <stempniak.adrian@gmail.com>
2021-01-16 17:20:59 +0100

    First commit -

---
+++ src/model/areaConverter.java
@@ -0,0 +1,12 @@
+package model;
+
+public class areaConverter {
+/*
+    int km2=1000000; //metrów kwadratowych
+    int m2=1;
+    int dm2=100 cm2=10000 mm2
+    int cm2=100 mm2
+    int ha=100 a=10000 m2
+    int a=100 m2
+*/
+}
---
+++ src/model/lengthConverter.java
@@ -0,0 +1,21 @@
+package model;
+
+public class lengthConverter {
+
+
+
+    double metr = 1;
+    double kilometr = 1000; //metrów
+    double decymetr = 0.1; //metrów
+    double centymetr = 0.01;  //metrów
+    double mikron = 0.000001;  //metrów
+    double cal = 0.0254; //metrów
+    double stopa = 0.3048; //metrów
+    double jard = 0.9144; //metrów
+    double sazen = 1.8288;  //metrów
+    double mila = 1609.344;  //metrów
+    double milaMorska = 1852;  //metrów
+    double kabel = 185.2;  //metrów
+
+
+}
